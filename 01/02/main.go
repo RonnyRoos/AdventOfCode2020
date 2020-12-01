6b5070d5d6781a87ca4b541beb65a7dcca8e05d9
Ronny Roos <ronny@ronnyroos.com>
2020-12-01 19:52:42 +0100

    First stab 2nd exercise

---
+++ 01/02/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("01/01/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var numbersInFile []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbersInFile = append(numbersInFile, i)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Iterate array
+	var found = false
+	for _, num := range numbersInFile {
+		for _, innerNum := range numbersInFile {
+			for _, numCeption := range numbersInFile {
+				if num+innerNum+numCeption == 2020 {
+					fmt.Println(num * innerNum * numCeption)
+					found = true
+					break
+				}
+
+				if found {
+					break
+				}
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Nice fancy repo for Advent of Code 2020 (https://adventofcode.com/2020/)
 
 ## 01
 ### 01
-```sh
+```console
 run go 01/01/main.go
 ```
 ### 02
-```sh
-run go 01/01/main.go
+```console
+run go 01/02/main.go
 ```
